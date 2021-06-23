3f0574c0c5824837237ba14e60508fea09bc05ae
manuu10 <manuel.alberding@web.de>
2021-06-23 12:57:22 +0200

    logout and logo goes to home

--- frontend/js/login_check.js
+++ frontend/js/login_check.js
@@ -1,5 +1,16 @@
+const doLogout = () => {
+  localStorage.removeItem("user");
+  window.location.reload();
+};
+
 if (localStorage.getItem("user") !== null) {
   console.log("is logged in");
   const item = document.getElementsByClassName("nav-group-authentication")[0];
   item.style.display = "none";
+  const logoutBtn = /*html */ `
+    <div class="nav-group">
+        <button onclick="doLogout()">Logout</button>
+    </div>`;
+
+  document.getElementsByTagName("navbar")[0].innerHTML += logoutBtn;
 }
