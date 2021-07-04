dc7c54b0f7198e8b137264d28e6169764a1c7880
TobiasSchoppen <Tobias.Schoppen@gmail.com>
2021-07-04 18:01:25 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -322,15 +322,9 @@ const numberOfCarsAvailableForType = async (typeId) => {
 app.listen(5431, async () => {
   console.log(`Server is Listening on 5431`);
   con = await mysql.createConnection({
-<<<<<<< HEAD
-    host: "localhost",
-    user: "root",
-    password: "123",
-=======
     host: "217.160.71.120",
     user: "adRental",
     password: "uX_S8fyRjv4qcJWV",
->>>>>>> 54da694881db8bfad86ec86f7f515adb46b85299
     database: "rental_portal",
   });
   const msg = await con.connect();
