4521e43351a52783047e9108c8ce4817a53016a7
manuu10 <manuel.alberding@web.de>
2021-07-04 20:59:59 +0200

    admin dashboard

--- frontend/js/login_check.js
+++ frontend/js/login_check.js
@@ -4,12 +4,15 @@ const doLogout = () => {
 };
 
 if (localStorage.getItem("user") !== null) {
+  const isAdmin = localStorage.getItem("user") === "admin";
   console.log("is logged in");
   const item = document.getElementsByClassName("nav-group-authentication")[0];
   item.style.display = "none";
+  const adminBtn = /*html */ `<a href="admin_dashboard.html"><button>Admin Dashbaord</button></a>`;
   const logoutBtn = /*html */ `
     <div class="nav-group">
         <button onclick="doLogout()">Logout</button>
+        ${isAdmin ? adminBtn : ""}
     </div>`;
 
   document.getElementsByTagName("navbar")[0].innerHTML += logoutBtn;
